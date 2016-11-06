be622e7e0174f480d861e6d7572eef32e7934c70
Tiancheng Zhu <phaitour@gmail.com>
2016-11-06 03:15:24 -0800

    small tweaks

--- lib/git_helper.rb
+++ lib/git_helper.rb
@@ -22,14 +22,19 @@
 #WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 GITIGNORE_CONTENTS = <<GITIGNORE
-# Xcode
+#
+## Mac Finder specific (in case it's not elsewhere)
+.DS_Store
+#
+#
+## Xcode
 #
 # gitignore contributors: remember to update Global/Xcode.gitignore, Objective-C.gitignore & Swift.gitignore
-
+#
 ## Build generated
 build/
 DerivedData/
-
+#
 ## Various settings
 *.pbxuser
 !default.pbxuser
@@ -40,51 +45,62 @@ DerivedData/
 *.perspectivev3
 !default.perspectivev3
 xcuserdata/
-
+#
 ## Other
 *.moved-aside
 *.xcuserstate
-
+#
 ## Obj-C/Swift specific
 *.hmap
 *.ipa
 *.dSYM.zip
 *.dSYM
-
-# CocoaPods
+#
+## Automatic backup files
+*~.nib/
+*.swp
+*~
+*.dat
+*.dep
+#
+#
+## CocoaPods
 #
 # We recommend against adding the Pods directory to your .gitignore. However
 # you should judge for yourself, the pros and cons are mentioned at:
 # https://guides.cocoapods.org/using/using-cocoapods.html#should-i-check-the-pods-directory-into-source-control
 #
-# Pods/
-
-# Carthage
+Pods/
+#
+#
+## Carthage
 #
 # Add this line if you want to avoid checking in source code from Carthage dependencies.
 # Carthage/Checkouts
-
-Carthage/Build
-
-# fastlane
+#
+# Carthage/Build
+#
+#
+## fastlane
 #
 # It is recommended to not store the screenshots in the git repo. Instead, use fastlane to re-generate the
 # screenshots whenever they are needed.
 # For more information about the recommended setup visit:
 # https://github.com/fastlane/fastlane/blob/master/fastlane/docs/Gitignore.md
-
+#
 fastlane/report.xml
 fastlane/Preview.html
 fastlane/screenshots
 fastlane/test_output
-
-# Code Injection
+#
+#
+## Code Injection
 #
 # After new code Injection tools there's a generated folder /iOSInjectionProject
 # https://github.com/johnno1962/injectionforxcode
-
+#
 iOSInjectionProject/
-
+#
 GITIGNORE
 
 GITATTRIBUTES_CONTENTS = '*.pbxproj binary merge=union'
